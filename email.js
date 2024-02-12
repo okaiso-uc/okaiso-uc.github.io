22a9a3eba4fe9f876f045c5c191082a31ab7c2cc
okaiso-uc <156627165+okaiso-uc@users.noreply.github.com>
2024-02-12 15:45:33 -0500

    Update email.js

--- email.js
+++ email.js
@@ -1,30 +1,15 @@
 var shown = false;
+var emailAddress = "okaiso@mail.uc.edu";
 
 function showhideEmail() {
-  if (shown) {
-    document.getElementById('email').textContent = "Show my email";
-    shown = false;
+  var emailSpan = document.getElementById('email');
+  if (!shown) {
+    var encodedEmail = encodeURI(emailAddress);
+    var myemail = "<a href='mailto:" + encodedEmail + "'>" + encodedEmail + "</a>";
+    emailSpan.innerHTML = myemail;
+    shown = true;
   } else {
-    var input = document.getElementById('emailInput').value;
-    var encodedInput = encodeInput(input);
-    if (validateEmail(encodedInput)) {
-      var encodedEmail = encodeURI(encodedInput);
-      var myemail = "<a href='mailto:" + encodedEmail + "'>" + encodedEmail + "</a>";
-      document.getElementById('email').innerHTML = myemail;
-      shown = true;
-    } else {
-      alert("Invalid email address!");
-    }
+    emailSpan.textContent = "Show my email";
+    shown = false;
   }
-}
-
-function encodeInput(input) {
-  const encoded = document.createElement('div');
-  encoded.innerText = input;
-  return encoded.innerHTML;
-}
-
-function validateEmail(email) {
-  const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-  return emailPattern.test(email);
 }
