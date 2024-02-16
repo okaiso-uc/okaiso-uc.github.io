ec706caa6a223ff649f3b0b089fb835fde989e73
okaiso-uc <156627165+okaiso-uc@users.noreply.github.com>
2024-02-15 20:47:10 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,111 +1,36 @@
+<script>
+    function updateThreatMeter(threatLevel) {
+    
+        var threatMeter = document.getElementById('threat-meter');
 
-particlesJS("particles-js", {
-  "particles": {
-    "number": {
-      "value": 80,
-      "density": {
-        "enable": true,
-        "value_area": 800
-      }
-    },
-    "color": {
-      "value": "#ffffff"
-    },
-    "shape": {
-      "type": "circle",
-      "stroke": {
-        "width": 0,
-        "color": "#000000"
-      },
-      "polygon": {
-        "nb_sides": 5
-      },
-      "image": {
-        "src": "img/github.svg",
-        "width": 100,
-        "height": 100
-      }
-    },
-    "opacity": {
-      "value": 0.5,
-      "random": false,
-      "anim": {
-        "enable": false,
-        "speed": 1,
-        "opacity_min": 0.1,
-        "sync": false
-      }
-    },
-    "size": {
-      "value": 3,
-      "random": true,
-      "anim": {
-        "enable": false,
-        "speed": 40,
-        "size_min": 0.1,
-        "sync": false
-      }
-    },
-    "line_linked": {
-      "enable": true,
-      "distance": 150,
-      "color": "#ffffff",
-      "opacity": 0.4,
-      "width": 1
-    },
-    "move": {
-      "enable": true,
-      "speed": 6,
-      "direction": "none",
-      "random": false,
-      "straight": false,
-      "out_mode": "out",
-      "bounce": false,
-      "attract": {
-        "enable": false,
-        "rotateX": 600,
-        "rotateY": 1200
-      }
-    }
-  },
-  "interactivity": {
-    "detect_on": "canvas",
-    "events": {
-      "onhover": {
-        "enable": true,
-        "mode": "grab"
-      },
-      "onclick": {
-        "enable": true,
-        "mode": "push"
-      },
-      "resize": true
-    },
-    "modes": {
-      "grab": {
-        "distance": 140,
-        "line_linked": {
-          "opacity": 1
+    
+        switch (threatLevel) {
+            case 'low':
+                threatMeter.style.width = '25%';
+                threatMeter.innerText = 'Low';
+                threatMeter.classList.remove('bg-warning', 'bg-danger');
+                threatMeter.classList.add('bg-success');
+                break;
+            case 'medium':
+                threatMeter.style.width = '50%';
+                threatMeter.innerText = 'Medium';
+                threatMeter.classList.remove('bg-success', 'bg-danger');
+                threatMeter.classList.add('bg-warning');
+                break;
+            case 'high':
+                threatMeter.style.width = '100%';
+                threatMeter.innerText = 'High';
+                threatMeter.classList.remove('bg-success', 'bg-warning');
+                threatMeter.classList.add('bg-danger');
+                break;
+            default:
+        
+                threatMeter.style.width = '25%';
+                threatMeter.innerText = 'Low';
+                threatMeter.classList.remove('bg-warning', 'bg-danger');
+                threatMeter.classList.add('bg-success');
         }
-      },
-      "bubble": {
-        "distance": 400,
-        "size": 40,
-        "duration": 2,
-        "opacity": 8,
-        "speed": 3
-      },
-      "repulse": {
-        "distance": 200,
-        "duration": 0.4
-      },
-      "push": {
-        "particles_nb": 4
-      },
-      "remove": {
-        "particles_nb": 2
-      }
     }
-  },
-  "retina_detect": true
-});
+
+    updateThreatMeter('high');
+</script>
